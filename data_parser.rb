ca86f39df99bdda9c7eb51ba10d2476e9c5c108a
Christian Neal <christianneal@Christians-MacBook-Air.local>
2015-09-04 15:20:45 -0500

    hard mode completed

--- data_parser.rb
+++ data_parser.rb
@@ -5,25 +5,19 @@ require 'erb'
 logs = []
 CSV.foreach("planet_express_logs.csv", headers: true) {|row| logs << row.to_hash }
 #^creates an array of hashes from the planet_express_logs
-#print logs
 
 trip_revenue = []
 trip_revenue = logs.map { |h| h["Money"]}
                    .map(&:to_i) #to convert string to integers
 
-total_revenue = trip_revenue.reduce(:+).to_s
+goal_revenue = 500000
 
-puts "1. We made " + total_revenue + " money this week."
+total_revenue = trip_revenue.reduce(:+).to_s
 
 fry_trips = []
 amy_trips = []
 bender_trips = []
 leela_trips = []
-
-# fry_bonus = #.1 * money from Earth bound trips
-# amy_bonus = #.1 * money from Mars bound trips
-# bender_bonus = #.1 * money from Uranus bound trips
-# leela_bonus = #.1 * money from all other bound trips
 
 logs.each do |h|
   if h["Destination"] == "Earth"
@@ -61,25 +55,65 @@ leela_money= leela_trips.map {|h| h["Money"]}
 
 leela_bonus = leela_money * 0.1
 
-puts "2. Bonuses:"
-puts "Fry gets " + fry_bonus.to_s + " as a bonus."
-puts "Amy gets " + amy_bonus.to_s + " as a bonus."
-puts "Bender gets " + bender_bonus.to_s + " as a bonus."
-puts "Leela gets " + leela_bonus.to_s + " as a bonus."
+mercury_trips=[]
+venus_trips=[]
+jupiter_trips=[]
+saturn_trips=[]
+neptune_trips=[]
+pluto_trips=[]
 
-puts "3. Trips"
+leela_trips.each do |h| #use leela because Fry, Amy, & Bender do all Earth, Mars & Uranus trips respectively
+  if h["Destination"] == "Mercury"
+    mercury_trips << h
+  elsif h["Destination"] == "Venus"
+    venus_trips << h
+  elsif h["Destination"] == "Jupiter"
+    jupiter_trips << h
+  elsif h["Destination"] == "Saturn"
+    saturn_trips << h
+  elsif h["Destination"] == "Neptune"
+    neptune_trips << h
+  else
+    pluto_trips << h
+  end
+end
 
-puts "Fry made " + fry_trips.count.to_s + " trips this month."
-puts "Amy made " + amy_trips.count.to_s + " trips this month."
-puts "Bender made " + bender_trips.count.to_s +  " trips this month."
-puts "Leela made " + leela_trips.count.to_s + " trips this month."
+mercury_money = mercury_trips.map {|h| h["Money"]}
+                             .map(&:to_i)
+                             .reduce(:+)
+
+venus_money = venus_trips.map {|h| h["Money"]}
+                         .map(&:to_i)
+                         .reduce(:+)
+
+earth_money = fry_money #because fry does all earth trips
+
+mars_money = amy_money #because amy does all mars trips
+
+jupiter_money = jupiter_trips.map {|h| h["Money"]}
+                             .map(&:to_i)
+                             .reduce(:+)
+
+saturn_money = saturn_trips.map {|h| h["Money"]}
+                           .map(&:to_i)
+                           .reduce(:+)
+
+uranus_money = bender_money #because bender does all uranus trips
+
+neptune_money = neptune_trips.map {|h| h["Money"]}
+                             .map(&:to_i)
+                             .reduce(:+)
+
+pluto_money = pluto_trips.map {|h| h["Money"]}
+                         .map(&:to_i)
+                         .reduce(:+)
+
+
 
 # all below is for the actual html part
 b = binding
 the_html = File.read("./report.erb")
 compiled_html = ERB.new(the_html).result(b)
-
-puts compiled_html
 
 File.open("./final.html", "wb") {|f| f << compiled_html }
 #makes sure that the  can read this file's variables
